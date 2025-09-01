f6535b6e0604159aa7bfc9f3edfec4c3096713da
Driann's <114148680+Driannm@users.noreply.github.com>
2025-09-01 21:23:37 +0700

    chore : install css-loader & CopyWebpackPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'production',
@@ -9,15 +10,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-      },
-    }),
-  ],
   module: {
     rules: [
       {
@@ -26,4 +18,18 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public", to: "" },
+      ],
+    }),
+  ],
 };
